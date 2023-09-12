bbda3cdb3cd9b95621cdedc04e6ee365eb04e8d4
vrlomhrn <vmahrian@gmail.com>
2023-09-12 21:42:51 +0700

    update brand icons

--- src/components/dynamic/card-brand.tsx
+++ src/components/dynamic/card-brand.tsx
@@ -3,9 +3,9 @@ import { CardProps } from "../../lib/@types/components.types";
 const Card = ({ image, name }: CardProps) => {
   return (
     <div className="flex flex-col items-center justify-center space-y-3 py-2 rounded w-[100px] h-[100px] bg-white px-2">
-      <img src={image} alt="techstack" width={48} />
+      <img src={image} alt="techstack" width={48} height={48}/>
 
-      <p className="text-sm text-center">{name}</p>
+      <p className="text-sm text-center font-medium">{name}</p>
     </div>
   );
 };
--- src/components/section/skills-section.tsx
+++ src/components/section/skills-section.tsx
@@ -1,5 +1,7 @@
 import Card from "../dynamic/card-brand";
 import NestJsIcon from "../../assets/images/nestjs.svg";
+import CSharp from "../../assets/images/c-sharp.png";
+import DotNet from "../../assets/images/dotnet.svg";
 
 const SkillsSection = () => {
   return (
@@ -10,31 +12,19 @@ const SkillsSection = () => {
         </h3>
         <div className="mt-2">
           <h5 className="text-[18px] font-medium underline underline-offset-4">
-            Programming Language
+            Programming Languages
           </h5>
 
           <div className="mt-2 max-w-full">
             <div className="flex flex-wrap gap-2 justify-center md:justify-start py-2">
-              <Card
-                image="https://img.icons8.com/color/48/000000/javascript.png"
-                name="Javascript"
-              />
-              <Card
-                image="https://img.icons8.com/color/48/000000/golang.png"
-                name="Go"
-              />
-              <Card
-                image="https://img.icons8.com/color/48/java-coffee-cup-logo--v1.png"
-                name="Java"
-              />
-              <Card
-                image="https://img.icons8.com/color/48/000000/python.png"
-                name="Python"
-              />
-              <Card
-                image="https://img.icons8.com/officel/80/php-logo.png"
-                name="PHP"
-              />
+              <Card image="https://img.icons8.com/color/48/000000/javascript.png" />
+              <Card image="https://img.icons8.com/color/48/000000/golang.png" />
+              <Card image="https://img.icons8.com/color/48/java-coffee-cup-logo--v1.png" />
+              <Card image="https://img.icons8.com/color/48/000000/python.png" />
+              <Card image="https://img.icons8.com/officel/80/php-logo.png" />
+              <Card image="https://img.icons8.com/color/48/dart.png" />
+              <Card image="https://img.icons8.com/color/48/c-plus-plus-logo.png" />
+              <Card image={CSharp} />
             </div>
           </div>
         </div>
@@ -45,69 +35,51 @@ const SkillsSection = () => {
 
           <div className="mt-2 max-w-full">
             <div className="flex flex-wrap gap-2 justify-center md:justify-start py-2">
-              <Card
-                image="https://img.icons8.com/color/48/nextjs.png"
-                name="Next"
-              />
-              <Card
-                image="https://img.icons8.com/color/48/nuxt-jc.png"
-                name="Nuxt"
-              />
-              <Card
-                image="https://img.icons8.com/fluency/48/laravel.png"
-                name="Laravel"
-              />
-              <Card
-                image="https://img.icons8.com/ios/50/express-js.png"
-                name="Express"
-              />
-              <Card
-                image="https://img.icons8.com/color/48/react-native.png"
-                name="React Native"
-              />
-              <Card image={NestJsIcon} name="Nest" />
+              <Card image="https://img.icons8.com/color/48/nextjs.png" />
+              <Card image="https://img.icons8.com/color/48/nuxt-jc.png" />
+              <Card image="https://img.icons8.com/fluency/48/laravel.png" />
+              <Card image="https://img.icons8.com/ios/50/express-js.png" />
+              <Card image="https://img.icons8.com/color/48/react-native.png" />
+              <Card image="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/48/external-codeigniter-is-an-open-source-software-rapid-development-web-framework-logo-shadow-tal-revivo.png" />
+              <Card image="https://img.icons8.com/color/48/flutter.png" />
+              <Card image={NestJsIcon} />
             </div>
           </div>
         </div>
         <div className="mt-2">
           <h5 className="text-[18px] font-medium underline underline-offset-4">
-            Misc
+            Tools & Miscellaneous
           </h5>
 
           <div className="mt-2 max-w-full">
             <div className="flex flex-wrap gap-2 justify-center md:justify-start py-2">
-              <Card
-                image="https://img.icons8.com/color/48/git.png"
-                name="Git"
-              />
-              <Card
-                image="https://img.icons8.com/material-outlined/48/github.png"
-                name="Github"
-              />
-              <Card
-                image="https://img.icons8.com/fluency/48/docker.png"
-                name="Docker"
-              />
-              <Card
-                image="https://img.icons8.com/color/48/linux--v1.png"
-                name="Linux"
-              />
-              <Card
-                image="https://img.icons8.com/color/48/amazon-web-services.png"
-                name="Amazon Web Services"
-              />
-              <Card
-                image="https://img.icons8.com/fluency/48/azure-1.png"
-                name="Azure"
-              />
-              <Card
-                image="https://img.icons8.com/color/48/typescript.png"
-                name="Typescript"
-              />
-              <Card
-                image="https://img.icons8.com/fluency/48/node-js.png"
-                name="NodeJs"
-              />
+              <Card image="https://img.icons8.com/color/48/git.png" />
+              <Card image="https://img.icons8.com/material-outlined/48/github.png" />
+              <Card image="https://img.icons8.com/fluency/48/docker.png" />
+              <Card image="https://img.icons8.com/color/48/linux--v1.png" />
+              <Card image="https://img.icons8.com/color/48/amazon-web-services.png" />
+              <Card image="https://img.icons8.com/fluency/48/azure-1.png" />
+              <Card image="https://img.icons8.com/color/48/typescript.png" />
+              <Card image="https://img.icons8.com/fluency/48/node-js.png" />
+              <Card image={DotNet} />
+              <Card image="https://img.icons8.com/color/48/graphql.png" />
+              <Card image="https://img.icons8.com/color/48/tailwindcss.png" />
+              <Card image="https://img.icons8.com/color-glass/48/bootstrap.png" />
+              <Card image="https://img.icons8.com/external-tal-revivo-color-tal-revivo/48/external-react-a-javascript-library-for-building-user-interfaces-logo-color-tal-revivo.png" />
+              <Card image="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/48/external-vuejs-an-open-source-javascript-framework-for-building-user-interfaces-and-single-page-applications-logo-shadow-tal-revivo.png" />
+            </div>
+          </div>
+        </div>
+        <div className="mt-2">
+          <h5 className="text-[18px] font-medium underline underline-offset-4">
+            Databases
+          </h5>
+
+          <div className="mt-2 max-w-full">
+            <div className="flex flex-wrap gap-2 justify-center md:justify-start py-2">
+              <Card image="https://img.icons8.com/color/48/mysql-logo.png" />
+              <Card image="https://img.icons8.com/color/48/postgreesql.png" />
+              <Card image="https://img.icons8.com/external-tal-revivo-color-tal-revivo/48/external-mongodb-a-cross-platform-document-oriented-database-program-logo-color-tal-revivo.png" />
             </div>
           </div>
         </div>
